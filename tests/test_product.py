8e3b43dbf46b31afbc7cc0e86131aa3ff9574184
Maxim Yuriev <yrev2011@gmail.com>
2024-11-27 12:52:45 +0700

    Edit test

--- tests/test_product.py
+++ tests/test_product.py
@@ -12,17 +12,20 @@ def product():
     }
     return product
 
+
 async def test_add_by_user(product: dict, client: AsyncClient, current_test_user: AsyncClient.cookies):
     response = await client.post("/product/add", json=product, cookies=current_test_user)
     body = response.json()
     assert response.status_code == 403
     assert body['detail'] == "Нет доступа!"
 
+
 async def test_add_by_admin(product: dict, client: AsyncClient, current_test_admin: AsyncClient.cookies):
     response = await client.post("/product/add", json=product, cookies=current_test_admin)
     body = response.json()
     assert response.status_code == 200
-    assert body['detail'] == "Продукт создан!"
+    assert body['detail'] == "Товар добавлен!"
+
 
 async def test_add_twice(product: dict, client: AsyncClient, current_test_admin: AsyncClient.cookies):
     product.update(name="product")
@@ -30,10 +33,9 @@ async def test_add_twice(product: dict, client: AsyncClient, current_test_admin:
     response = await client.post("/product/add", json=product, cookies=current_test_admin)
     body = response.json()
     assert response.status_code == 200
-    assert body['detail'] == "Продукт создан!"
+    assert body['detail'] == "Товар добавлен!"
 
     response = await client.post("/product/add", json=product, cookies=current_test_admin)
     body = response.json()
     assert response.status_code == 400
     assert body['detail'] == "Товар уже существует!"
-
