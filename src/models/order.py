504e43cea0cff53ee047a6a95b2dd3786ad7fcb4
Maxim Yuriev <yrev2011@gmail.com>
2024-12-06 19:32:12 +0700

    Exchange order models

--- src/models/order.py
+++ src/models/order.py
@@ -1,4 +1,5 @@
 import datetime
+import uuid
 
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -10,12 +11,16 @@ class OrderProduct(Base):
     __tablename__ = "order_product"
     order_id: Mapped[int] = mapped_column(ForeignKey("order.order_id", ondelete="CASCADE"), primary_key=True)
     product_id: Mapped[int] = mapped_column(ForeignKey("product.product_id", ondelete="CASCADE"), primary_key=True)
+    product_count: Mapped[int]
+    product_on_order_price: Mapped[int]
 
+    product: Mapped["Product"] = relationship()
 
 class Order(Base):
     __tablename__ = "order"
     order_id: Mapped[int] = mapped_column(primary_key=True)
-    amount: Mapped[int]
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.user_id"))
+    order_price: Mapped[int]
     created_at: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.utcnow())
 
-    products: Mapped[list["Product"]] = relationship(secondary="order_product")
+    products: Mapped[list["OrderProduct"]] = relationship()
