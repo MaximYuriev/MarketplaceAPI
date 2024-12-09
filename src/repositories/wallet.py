d9241b9eababa3015718ee07ce4baab4e3c1dc46
Maxim Yuriev <yrev2011@gmail.com>
2024-12-09 12:51:46 +0700

    Update wallet repository

--- src/repositories/wallet.py
+++ src/repositories/wallet.py
@@ -12,3 +12,10 @@ class WalletRepository:
     def create(self, user_id: uuid.UUID | str):
         wallet = Wallet(user_id=user_id)
         self.session.add(wallet)
+
+    async def get(self, user_id: uuid.UUID | str) -> Wallet | None:
+        return await self.session.get(Wallet, user_id)
+
+    def update(self, wallet: Wallet, update_data: dict):
+        for key, value in update_data.items():
+            setattr(wallet, key, value)
