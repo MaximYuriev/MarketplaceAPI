a1a3308e614fd9217de781afa48dbdf344827b72
Maxim Yuriev <yrev2011@gmail.com>
2024-12-10 13:05:12 +0700

    Edit user repository

--- src/repositories/wallet.py
+++ src/repositories/wallet.py
@@ -1,12 +1,15 @@
 import uuid
+from typing import Annotated
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from db import get_session
 from models.wallet import Wallet
 
 
 class WalletRepository:
-    def __init__(self, session: AsyncSession):
+    def __init__(self, session: Annotated[AsyncSession, Depends(get_session)]):
         self.session = session
 
     def create(self, user_id: uuid.UUID | str):
@@ -16,6 +19,8 @@ class WalletRepository:
     async def get(self, user_id: uuid.UUID | str) -> Wallet | None:
         return await self.session.get(Wallet, user_id)
 
-    def update(self, wallet: Wallet, update_data: dict):
+    async def update(self, wallet: Wallet, update_data: dict) -> Wallet:
         for key, value in update_data.items():
             setattr(wallet, key, value)
+        await self.session.commit()
+        return wallet
